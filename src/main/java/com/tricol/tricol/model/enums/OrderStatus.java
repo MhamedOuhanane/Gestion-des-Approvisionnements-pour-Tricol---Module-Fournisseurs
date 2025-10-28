c6509dd574fd59cdee8df367d775b740c423fd72
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-10-28 23:41:06 +0100

    GDAPT-17 cree les tables de la base de donnee a partire de les migrations utilisant liquibase

--- src/main/java/com/tricol/tricol/model/enums/OrderStatus.java
+++ src/main/java/com/tricol/tricol/model/enums/OrderStatus.java
@@ -1,4 +1,18 @@
 package com.tricol.tricol.model.enums;
 
 public enum OrderStatus {
+    PENDING("En attente"),
+    VALIDATED("Validé"),
+    DELIVERED("Livré"),
+    CANCELED("Annulé");
+
+    private final String desc;
+
+    OrderStatus(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
 }
