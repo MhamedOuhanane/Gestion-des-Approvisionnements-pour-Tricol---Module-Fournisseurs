9bd76ad15986c6192115ecc5d4c85c3dd7984b5e
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-10-29 14:13:11 +0100

    GDAPT-25 Créer le FournisseurRepository (Spring Data JPA).

--- src/main/java/com/tricol/tricol/model/entity/Supplier.java
+++ src/main/java/com/tricol/tricol/model/entity/Supplier.java
@@ -3,7 +3,6 @@ package com.tricol.tricol.model.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
--- src/main/java/com/tricol/tricol/repository/SupplierRepository.java
+++ src/main/java/com/tricol/tricol/repository/SupplierRepository.java
@@ -1,4 +1,24 @@
 package com.tricol.tricol.repository;
 
-public interface SupplierRepository {
+import com.tricol.tricol.model.entity.Supplier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, UUID> {
+    Page<Supplier> findByCityContainingAndContactContainingAndCompanyContaining(String city, String contact, String company, Pageable pageable);
+    Page<Supplier> findByCityContainingAndContactContaining(String city, String contact, Pageable pageable);
+    Page<Supplier> findByCityContainingAndCompanyContaining(String city, String company, Pageable pageable);
+    Page<Supplier> findByContactContainingAndCompanyContaining(String contact, String company, Pageable pageable);
+    Page<Supplier> findByCityContaining(String city, Pageable pageable);
+    Page<Supplier> findByContactContaining(String contact, Pageable pageable);
+    Page<Supplier> findByCompanyContaining(String company, Pageable pageable);
+    Page<Supplier> findSuppliers(Pageable pageable);
+    Optional<Supplier> findByEmail(String email);
+    Optional<Supplier> findByICE(String ice);
 }
