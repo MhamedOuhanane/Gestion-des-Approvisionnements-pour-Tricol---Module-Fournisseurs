9fd1736f3d9c286fd56c6e215900804a561b3585
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-10-29 10:16:46 +0100

    GDAPT-17 Modifier les migations utilisers pour la creation des tables de base de donne

--- src/main/java/com/tricol/tricol/model/enums/StockMovementType.java
+++ src/main/java/com/tricol/tricol/model/enums/StockMovementType.java
@@ -1,4 +1,18 @@
 package com.tricol.tricol.model.enums;
 
 public enum StockMovementType {
+    PENDING("En attente"),
+    VALIDATED("Validé"),
+    DELIVERED("Livré"),
+    CANCELED("Annulé");
+
+    private final String desc;
+
+    StockMovementType(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
 }
