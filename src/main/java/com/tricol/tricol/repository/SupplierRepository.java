034fc8ad7f92950911d32cc98eb6c0ea3ede486d
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-10-30 14:28:35 +0100

    GDAPT-26 Créer le service du Fournisseur et son implémentation (CRUD).

--- src/main/java/com/tricol/tricol/repository/SupplierRepository.java
+++ src/main/java/com/tricol/tricol/repository/SupplierRepository.java
@@ -12,12 +12,6 @@ import java.util.UUID;
 @Repository
 public interface SupplierRepository extends JpaRepository<Supplier, UUID> {
     Page<Supplier> findByCityContainingAndContactContainingAndCompanyContaining(String city, String contact, String company, Pageable pageable);
-    Page<Supplier> findByCityContainingAndContactContaining(String city, String contact, Pageable pageable);
-    Page<Supplier> findByCityContainingAndCompanyContaining(String city, String company, Pageable pageable);
-    Page<Supplier> findByContactContainingAndCompanyContaining(String contact, String company, Pageable pageable);
-    Page<Supplier> findByCityContaining(String city, Pageable pageable);
-    Page<Supplier> findByContactContaining(String contact, Pageable pageable);
-    Page<Supplier> findByCompanyContaining(String company, Pageable pageable);
     Optional<Supplier> findByEmail(String email);
     Optional<Supplier> findByIce(String ice);
 }
--- src/main/java/com/tricol/tricol/service/impl/SupplierServiceImpl.java
+++ src/main/java/com/tricol/tricol/service/impl/SupplierServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tricol.tricol.service.impl;
 
+import com.tricol.tricol.exception.AppException;
 import com.tricol.tricol.model.dto.SupplierDTO;
 import com.tricol.tricol.model.entity.Supplier;
 import com.tricol.tricol.model.mapper.SupplierMapper;
@@ -7,7 +8,10 @@ import com.tricol.tricol.repository.SupplierRepository;
 import com.tricol.tricol.service.interfaces.Service;
 import com.tricol.tricol.service.interfaces.SupplierService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.http.HttpStatus;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
@@ -24,83 +28,127 @@ public class SupplierServiceImpl implements Service<SupplierDTO>, SupplierServic
 
     @Override
     public Map<String, Object> create(SupplierDTO dto) {
-        if (dto == null) {
-            return Map.of(
-                    "message", "Les informations de fournisseur ne peut pas étre vides",
-                    "statusCode", 201,
-                    "supplier", Optional.empty());
+        if (dto == null) throw new AppException("Les informations de fournisseur ne peut pas étre vides", HttpStatus.BAD_REQUEST);
+        if (supplierRepository.findByEmail(dto.getEmail()).isPresent())  {
+            throw new AppException(
+                    "Un fournisseur avec l'email '" + dto.getEmail() + "' existe déjà.",
+                    HttpStatus.CONFLICT
+            );
         }
-        try {
-            Supplier supplier = supplierRepository.save(supplierMapper.toEntity(dto));
-            return Map.of("message", "Le fournisseur de mail '" + supplier.getEmail() + "' a été ajouté avec succès !",
-                    "statusCode", 201,
-                    "supplier", supplierMapper.toDto(supplier));
-        } catch (RuntimeException e) {
-            return Map.of(
-                    "error", e.getMessage(),
-                    "statusCode", 500,
-                    "supplier", dto);
-        }
+        Supplier supplier = supplierRepository.save(supplierMapper.toEntity(dto));
+        return Map.of("message", "Le fournisseur de mail '" + supplier.getEmail() + "' a été ajouté avec succès !",
+                "status", 201,
+                "supplier", supplierMapper.toDto(supplier));
     }
 
     @Override
     public Map<String, Object> findById(UUID uuid) {
-        if (uuid == null) {
-            return Map.of(
-                    "message", "L'identifiant du fournisseur ne peut pas être vide",
-                    "statusCode", 400,
-                    "supplier", Optional.empty()
-            );
-        }
-        try {
-            Optional<Supplier> optionalSupplier = supplierRepository.findById(uuid);
-
-            if (optionalSupplier.isEmpty()) {
-                return Map.of(
-                        "message", "Aucun fournisseur trouvé avec cet identifiant",
-                        "statusCode", 404,
-                        "supplier", Optional.empty()
-                );
-            }
-
-            Supplier supplier = optionalSupplier.get();
-            return Map.of(
-                    "message", "Fournisseur trouvé avec succès",
-                    "statusCode", 200,
-                    "supplier", supplierMapper.toDto(supplier)
-            );
-
-        } catch (RuntimeException e) {
-            return Map.of(
-                    "error", e.getMessage(),
-                    "statusCode", 500,
-                    "supplier", Optional.empty()
-            );
-        }
+        if (uuid == null) throw new AppException("L'identifiant du fournisseur ne peut pas être vide", HttpStatus.BAD_REQUEST);
+        Supplier supplier = supplierRepository.findById(uuid)
+                .orElseThrow(() -> new AppException("Aucun fournisseur trouvé avec cet identifiant", HttpStatus.NOT_FOUND));
+        return Map.of(
+                "message", "Fournisseur trouvé avec succès",
+                "status", 200,
+                "supplier", supplierMapper.toDto(supplier)
+        );
     }
 
     @Override
     public Map<String, Object> findAll(Map<String, Object> map) {
-        return Map.of();
+        if (map.isEmpty()) {
+            return Map.of("page", 0, "size", 5);
+        }
+        Pageable pageable = PageRequest.of((int) map.get("page"), (int) map.get("size"), Sort.by("contact").ascending());
+        String city = (String) map.getOrDefault("city", "");
+        String company = (String) map.getOrDefault("company", "");
+        String contact = (String) map.getOrDefault("contact", "");
+        Page<Supplier> suppliers = suppliers = supplierRepository.findByCityContainingAndContactContainingAndCompanyContaining(city, contact, company, pageable);
+
+        String message;
+        Object data;
+        if (suppliers.isEmpty()) {
+            message = "Aucun fournisseur n'existe dans le système";
+            data = suppliers.stream()
+                    .map(supplierMapper::toDto)
+                    .toList();
+        } else {
+            message = "Tous les fournisseurs trouvés avec succès";
+            data = List.of();
+        }
+
+        return Map.of(
+                "message", message,
+                "status", 200,
+                "data", data
+        );
     }
 
     @Override
     public Map<String, Object> update(UUID uuid, SupplierDTO dto) {
-        return Map.of();
+        if (uuid == null) throw new AppException("L'identifiant du fournisseur ne peut pas être vide", HttpStatus.BAD_REQUEST);
+        if (dto == null) throw new AppException("Les informations de fournisseur ne peut pas étre vides", HttpStatus.BAD_REQUEST);
+        Supplier supplier = supplierRepository.findById(uuid)
+                .orElseThrow(() -> new AppException("Aucun fournisseur trouvé avec cet identifiant", HttpStatus.NOT_FOUND));
+        if (!supplier.getEmail().equals(dto.getEmail()) && supplierRepository.findByEmail(dto.getEmail()).isPresent()) {
+            throw new AppException(
+                    "Un fournisseur avec l'email '" + dto.getEmail() + "' existe déjà.",
+                    HttpStatus.CONFLICT,
+                    supplier
+            );
+        }
+        supplier.setCompany(dto.getCompany());
+        supplier.setContact(dto.getContact());
+        supplier.setEmail(dto.getEmail());
+        supplier.setIce(dto.getIce());
+        supplier.setCity(dto.getCity());
+        supplier.setAddress(dto.getAddress());
+        supplier = supplierRepository.save(supplierMapper.toEntity(dto));
+        return Map.of(
+                "message", "Le fournisseur '" + supplier.getEmail() + "' a été mis à jour avec succès !",
+                "status", 200,
+                "data", supplierMapper.toDto(supplier)
+        );
     }
 
     @Override
     public Map<String, Object> delete(UUID uuid) {
-        return Map.of();
+        if (uuid == null) throw new AppException("L'identifiant du fournisseur ne peut pas être vide", HttpStatus.BAD_REQUEST);
+        Supplier supplier = supplierRepository.findById(uuid)
+                .orElseThrow(() -> new AppException("Aucun fournisseur trouvé avec cet identifiant", HttpStatus.NOT_FOUND));
+        supplierRepository.delete(supplier);
+        boolean deleted = supplierRepository.findById(uuid).isPresent();
+        String message = deleted
+                ? "Le fournisseur a été supprimé avec succès."
+                : "Échec de la suppression du fournisseur.";
+        int status = deleted ? 200 : 500;
+        return Map.of(
+                "message", message,
+                "status", status,
+                "data", deleted ? Optional.empty() : supplierMapper.toDto(supplier)
+        );
     }
 
     @Override
     public Map<String, Object> findByEmail(String email) {
-        return Map.of();
+        if (email == null) throw new AppException("L'address mail du fournisseur ne peut pas être vide", HttpStatus.BAD_REQUEST);
+        Supplier supplier = supplierRepository.findByEmail(email)
+                .orElseThrow(() -> new AppException("Aucun fournisseur trouvé avec cet identifiant", HttpStatus.NOT_FOUND));
+        return Map.of(
+                "message", "Fournisseur trouvé avec succès",
+                "status", 200,
+                "supplier", supplierMapper.toDto(supplier)
+        );
     }
 
     @Override
     public Map<String, Object> findByIce(String ice) {
-        return Map.of();
+        if (ice == null) throw new AppException("L'ICE du fournisseur ne peut pas être vide", HttpStatus.BAD_REQUEST);
+        Supplier supplier = supplierRepository.findByIce(ice)
+                .orElseThrow(() -> new AppException("Aucun fournisseur trouvé avec cet identifiant", HttpStatus.NOT_FOUND));
+        return Map.of(
+                "message", "Fournisseur trouvé avec succès",
+                "status", 200,
+                "supplier", supplierMapper.toDto(supplier)
+        );
     }
 }
